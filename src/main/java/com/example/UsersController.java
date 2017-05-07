a8b1ec8ca2aabf73dc2fa1acf4989f21a05c75fd
草苅　快 <kusakai@kusakai-no-MacBook-Air.local>
2017-05-07 18:07:28 +0900

    binally uploader sample

--- src/main/java/com/example/UsersController.java
+++ src/main/java/com/example/UsersController.java
@@ -1,5 +1,17 @@
 package com.example;
 
+import static com.example.util.ImageTypeCheck.getFormat;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import javax.transaction.Transactional;
@@ -48,5 +60,26 @@ public class UsersController {
 	@RequestMapping(path = "/users/{id}", method = RequestMethod.DELETE)
 	public void destory(Model model, @PathVariable("id") int id) {
 		repository.delete(id);
+	}
+
+	@RequestMapping(path = "/users/upload", method = RequestMethod.PUT)
+	public void upload(InputStream req) throws IOException {
+
+		ByteArrayOutputStream byteos = new ByteArrayOutputStream();
+		byte[] buf = new byte[1024];
+		int size = 0;
+		while ((size = req.read(buf, 0, buf.length)) != -1) {
+			byteos.write(buf, 0, size);
+		}
+
+		String filename = DateTimeFormatter.ofPattern("yyyyMMddHHmmssSSS").format(LocalDateTime.now());
+		Path uploadfile = Paths.get("/Users/kusakai/Documents/workspace-sts-3.8.4.RELEASE/demo-kusa/image/" + filename
+				+ "." + getFormat(byteos.toByteArray()));
+
+		try (OutputStream os = Files.newOutputStream(uploadfile, StandardOpenOption.CREATE)) {
+			os.write(byteos.toByteArray());
+		} catch (IOException ex) {
+			System.err.println(ex);
+		}
 	}
 }
